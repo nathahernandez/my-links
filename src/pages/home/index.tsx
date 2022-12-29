a897deb6894ef70c166345c2aa99be2e44aff3fd
Nathã Hernandez <nathahcorrea@gmail.com>
2022-12-29 08:50:49 -0300

    feat: created initial page

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,11 @@
+import { Container } from './styles'
+import Content from '../../components/Content'
+
+export default function Home () {
+	return (
+		<Container>
+			<Content />
+		</Container>
+	)
+
+}
